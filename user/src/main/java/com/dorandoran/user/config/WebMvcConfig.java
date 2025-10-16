ab017d57f69d912e9368f8bf83fa2be9d196f78d
HongmookChoi <leochoi224@gmail.com>
2025-10-16 23:06:22 +0900

    chore(user): CORS 전역 허용 및 HMAC 인증 임시 우회

--- user/src/main/java/com/dorandoran/user/config/WebMvcConfig.java
+++ user/src/main/java/com/dorandoran/user/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.dorandoran.user.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +15,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(hmacAuthInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
 
